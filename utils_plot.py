8a3915b200da41c85fcf4d7a93c9613f9298313c
naomi fridman <naomi.fridman@gmail.com>
2019-01-29 09:48:27 +0200

    added few function

--- utils_plot.py
+++ utils_plot.py
@@ -101,3 +101,56 @@ def drow_history(history, metric):
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()    
+#--------------------------------------------------------------------------------
+import matplotlib.pyplot as plt
+
+def show_list_images_and_predictions(imgs, preds, title1 = 'image',title2 = 'prediction'):
+    n_img = len(imgs)
+    fig, m_axs = plt.subplots(2, n_img, figsize = (n_img*2, 4))
+    i = 0
+    for (c_im, c_lab) in m_axs.T:
+        c_im.imshow(imgs[i])
+        c_im.axis('off')
+        c_im.set_title(title1)
+
+        c_lab.imshow(y_train[ind])
+        c_lab.axis('off')
+        c_lab.set_title(title2) 
+        i+=1
+        
+def show_list_images(imgs, titles=None):
+    n_img = len(imgs)
+    fig, m_axs = plt.subplots(1, n_img, figsize = (n_img*2, 4))
+    i = 0
+    for (c_im) in m_axs.T:
+        c_im.imshow(imgs[i])
+        c_im.axis('off')
+        if (titles is not None):
+            c_im.set_title(titles[i])
+        i+=1
+
+def plot_img_grid_array(imgs, n_col=4,n_row=4, titles=None):
+
+    fig, axs = plt.subplots(n_row, n_col)
+    l=0
+    for i in range(n_col):
+        for j in range(n_row):
+
+            axs[i,j].imshow(imgs[:,:,l])
+            if(titles is not None):
+                axs[i,j].set_title(titles[l])
+            axs[i,j].axis('off')
+            l+=1
+
+def plot_img_grid_list(imgs, n_col=4,n_row=4, titles=None):
+
+    fig, axs = plt.subplots(n_row, n_col)
+    l=0
+    for i in range(n_col):
+        for j in range(n_row):
+
+            axs[i,j].imshow(imgs[l])
+            if(titles is not None):
+                axs[i,j].set_title(titles[l])
+            axs[i,j].axis('off')
+            l+=1
